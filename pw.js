733ca9599176a9187c01705dffc0d3a9f4400f86
Sierra <sierra@Ryans-MacBook-Pro.local>
2021-06-23 19:07:21 -0700

    added files and assets from the project downloaded from google drive. initial commit

---
+++ pw.js
@@ -0,0 +1,22 @@
+// alert("Ben is responsible for Mt Etna being unruly rn.");
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
